846caeb3b88f55090e2ed9289877cdd173783a5d
Luis Deschamps Rudge <luis@luisrudge.net>
2019-05-31 10:30:03 -0500

    adding list queries

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const authors = require("./authors.json");
 const books = require("./books.json");
 
+const debug = (...params) => console.log(params);
+
 module.exports.getBooks = async () => {
+  debug("getBooks");
   return books;
 };
 module.exports.getBook = async id => {
@@ -11,6 +14,7 @@ module.exports.getAuthors = async () => {
   return authors;
 };
 module.exports.getAuthor = async id => {
+  debug("getAuthor", id);
   return authors.find(a => a.id === id);
 };
 module.exports.likeBook = async id => {
--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const typeDefs = gql`
   type Book {
     id: Int!
     title: String!
+    rating: Float!
+    year: Int!
+    image: String!
+    likes: Int!
+    authorId: Int!
+    author: Author!
   }
+  type Author {
+    id: Int!
+    name: String!
+    imageUrl: String!
+    about: String!
+  }
+
   type Query {
     books: [Book]
   }
 `;
 
 const resolvers = {
+  Book: {
+    author: async (book) => await db.getAuthor(book.authorId)
+  },
   Query: {
     books: async () => await db.getBooks()
   }
